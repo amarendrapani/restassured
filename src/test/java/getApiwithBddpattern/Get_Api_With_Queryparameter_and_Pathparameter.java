d286372ff1cb4adba8f31946f288f994661f7eea
Amarendra Pani <amarendra.pani@raybiztech.com>
2024-11-26 15:24:19 +0530

    >added how to pass path parameter with data provider and without data provider and also added data provider for passing multiple query parameters.

--- src/test/java/getApiwithBddpattern/Get_Api_With_Queryparameter_and_Pathparameter.java
+++ src/test/java/getApiwithBddpattern/Get_Api_With_Queryparameter_and_Pathparameter.java
@@ -10,6 +10,7 @@ import static io.restassured.RestAssured.given;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
@@ -23,7 +24,7 @@ public class Get_Api_With_Queryparameter_and_Pathparameter {
 		RestAssured.baseURI = "https://gorest.co.in/"; 
 		
 		given().log().all()
-		.header("header","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
+		.header("Authorization","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
 		
 			.queryParam("name", "trivedi")
 				.queryParam("status", "Active")
@@ -50,7 +51,7 @@ public class Get_Api_With_Queryparameter_and_Pathparameter {
 		  queryparms.put("gender", "male");
 		
 		given().log().all()
-		.header("header","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
+		.header("Authorization","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
 		
 			.queryParams(queryparms)
 					.when().log().all()
@@ -78,7 +79,7 @@ public class Get_Api_With_Queryparameter_and_Pathparameter {
 	
 	
 		given().log().all()
-		.header("header","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
+		.header("Authorization","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
 	
 				.queryParams(queryparms)
 					.when().log().all()
@@ -88,8 +89,114 @@ public class Get_Api_With_Queryparameter_and_Pathparameter {
 									.statusCode(200)
 										.and()
 											.contentType(ContentType.JSON);
+		
+		
+	}
 	
-}
+	
+	@DataProvider
+	public Object[][] getUsername_and_Status(){
+		return new Object[][] {	
+			{"trivedi","active"},
+			{"trivedi","inactive"}
+		};
+	}
+	
+	
+	@Test(dataProvider = "getUsername_and_Status")
+	public void getuser_With_Queryparams_Using_Dataprovider(String name,String status) {
+	
+		RestAssured.baseURI = "https://gorest.co.in/"; 
+	
+		 Map<String,String> queryparms = new HashMap<String,String>();
+		  queryparms.put("name", name);
+		  queryparms.put("status", status);
+	
+	
+		given().log().all()
+		.header("Authorization","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
+	
+				.queryParams(queryparms)
+					.when().log().all()
+						.get("/public/v2/users")
+							.then().log().all()
+								.assertThat()
+									.statusCode(200)
+										.and()
+											.contentType(ContentType.JSON);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		//==========================================================================================================
+		//============================ Path Parameters handling ====================================================
+		//==========================================================================================================
+		
+		@Test
+		public void getUserApi_With_Pathparameter() {
+			
+			RestAssured.baseURI = "https://gorest.co.in/";
+			
+			given().log().all()
+				.header("Authorization","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
+				
+					.pathParam("userid","7381532")
+						.when().log().all()
+							.get("/public/v2/users/{userid}/posts")
+								.then().log().all()
+									.assertThat()
+										.statusCode(200);
+			
+			
+		}
+		
+		
+		// by using data provider
+		
+		@DataProvider
+		public Object[][] getUserId(){
+			return new Object[][] {
+				
+				{"6942516"},{"7589658"},{"5285698"}
+				
+				};
+				
+		}
+		
+		@Test(dataProvider = "getUserId")
+         public void getUserApi_With_Pathparameter_WithDataProvider(String userid) {
+			
+			RestAssured.baseURI = "https://gorest.co.in/";
+			
+			given().log().all()
+				.header("Authorization","Bearer fffcb18cd760cbb70acb4190be85c2996d3be5d2a802c25e1dfb088666bc3821")
+				
+					.pathParam("userid",userid)
+						.when().log().all()
+							.get("/public/v2/users/{userid}/posts")
+								.then().log().all()
+									.assertThat()
+										.statusCode(200);
+			
+			
+		}
+		
+	
+
 	
 
 }
